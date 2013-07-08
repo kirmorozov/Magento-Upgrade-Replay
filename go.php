e06fa76f489500132f55b5a90cb9a8aace34b5bd
Alexey Mischenko <mover87@gmail.com>
2013-07-08 15:52:41 +0300

    new commit

---
+++ go.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Mover
+ * Date: 08.07.13
+ * Time: 15:42
+ * To change this template use File | Settings | File Templates.
+ */
+echo('go');
+?>
