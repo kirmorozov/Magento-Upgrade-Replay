ca7ed58b3e7c140c76b7ddc3aa8e05ef5e8c3d8d
Alexey Mischenko <mover87@gmail.com>
2013-07-08 16:06:04 +0300

    header fix

--- go.php
+++ go.php
@@ -1,10 +1,3 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Mover
- * Date: 08.07.13
- * Time: 15:42
- * To change this template use File | Settings | File Templates.
- */
 echo('go');
 ?>
